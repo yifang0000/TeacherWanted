fcb1bc70e5cf14e47b06c36e582a7b3c9465c37c
Michael <tioni811@gmail.com>
2023-05-22 10:56:38 +0800

    修正:專案 修正JDK

--- src/main/java/com/example/teacherwanted/TeacherWantedApplication.java
+++ src/main/java/com/example/teacherwanted/TeacherWantedApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeacherWantedApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TeacherWantedApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherWantedApplication.class, args);
+	}
 
 }
