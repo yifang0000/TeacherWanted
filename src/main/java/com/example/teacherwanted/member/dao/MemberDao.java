5b7bc17e9fcbf60820391307b18fd858240d0bb7
TingLun <lisawang0219@gmail.com>
2023-06-01 19:55:19 +0800

    member物件,會員中心

--- src/main/java/com/example/teacherwanted/member/dao/MemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.teacherwanted.member.dao;
-
-import com.example.teacherwanted.core.dao.CoreDao;
-import com.example.teacherwanted.member.model.Member;
-
-public interface MemberDao extends CoreDao<Member,Integer,String> {
-
-        Member selectById(Integer id);
-}
