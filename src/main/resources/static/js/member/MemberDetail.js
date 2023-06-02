1746e05fd5e2d75f6f70970e42e49e21d7e5a46e
TingLun <lisawang0219@gmail.com>
2023-06-02 19:47:51 +0800

    修改:member物件,會員中心

--- src/main/resources/static/js/member/MemberDetail.js
+++ src/main/resources/static/js/member/MemberDetail.js
@@ -3,38 +3,17 @@
 $(document).ready(function() {
     $('#calendar').fullCalendar();
 
-
   });
+  $('#calendar').fullCalendar(); 
+//===========頭貼上傳===========//
+const btn = document.querySelector('#detail_submit');
+const file = document.querySelector('#p_file');
 
+// file.addEventListener('change', () => {
+//   const service_img = document.querySelector('#service_img');
+//   service_img.src = URL.createObjectURL(file.files[0]);
+// });
 
-// $('#calendar').fullCalendar({
-//     editable: true,
-//     events: [{
-//       title: '昨天的活動',
-//       start: moment().subtract(1, 'days').format('YYYY-MM-DD')
-//     }, {
-//       title: '持續一周的活動',
-//       start: moment().add(7, 'days').format('YYYY-MM-DD'),
-//       end: moment().add(14, 'days').format('YYYY-MM-DD'),
-//       color: 'lightBlue'
-//     }]
-//   });
-// // 創建一个Moment對象
-// var now = moment();
-
-// // 解析日期字符串
-// var date = moment('2023-05-22', 'YYYY-MM-DD');
-
-// // 格式化日期
-// var formattedDate = date.format('YYYY-MM-DD');
-
-// // 進行日期運算
-// var futureDate = date.add(1, 'week');
-
-// // 獲取日期的部分信息
-// var year = date.year();
-// var month = date.month();
-// var day = date.date();
 
 //==========資料送回前端============//
 //========================//
@@ -61,25 +40,26 @@ $(document).ready(function() {
 
 
 
-// function axiosGetMember(memId) {
-//   return axios
-//     .get("/memberDetail", {
-//       params: {
-//         memId: memId
-//       }
-//     })
-//     .then((res) => {
-//       console.log("------------>", res.data);
-//       return res.data;
-//     })
-//     .catch((err) => {
-//       console.log("沒有資料喔!");
-//       // console.error(err.response.data);
-//       // alert(err.response.data);
-//       // window.location.href = "/member/MemberDetail.html";
-//     });
-// }
+function axiosGetMember(memId) {
+  return axios
+    .get("http://localhost:8080/memberDetail", { 
+      params: { 
+        memId: memId 
+      } 
+    })
+    .then((res) => {
+      console.log("------------>", res.data);
+      return res.data;
+    })
+    .catch((err) => {
+      console.log("沒有資料喔!");
+      // console.error(err.response.data);
+      // alert(err.response.data);
+      // window.location.href = "/member/MemberDetail.html";
+    });
+}
 
+var data;
 
 //===========使用Vue  =============//
  const app = Vue.createApp({          
@@ -104,7 +84,9 @@ $(document).ready(function() {
         createTime: "",
         updateTime: "",
         memStatus: "",
-        member:"",
+        member:[],
+        isDisabled: true
+        
       };
     },
     mounted() {
@@ -112,85 +94,107 @@ $(document).ready(function() {
       // const url= "http://localhost:8080/memberInfo";
       // const urlParams = new URLSearchParams(window.location.search);
       // const memId = urlParams.get("memId");
-      // console.log(this.memId);
-      axios.post(this.url,{                            //promise 等後端回應
-        headers: {
-          'Content-Type': 'application/json'
-      },
-        memId :this.memId,
-        memAccount: this.memAccount,
-        memPassword: this.memPassword,
-        memName: this.memName,
-        memPhone: this.memPhone,
-        memNickname: this.memNickname,
-        memBirthday: this.memBirthday,
-        memGender: this.memGender,
-        memEmail: this.memEmail,
-        mailVerify: this.mailVerify,
-        memLocation: this.memLocation,
-        memPhoto: this.memPhoto,
-        interest1: this.interest1,
-        interest2: this.interest2,
-        interest3: this.interest3,
-        createTime: this.createTime,
-        updateTime: this.updateTime,
-        memStatus: this.memStatus
-    
-    })
-      .then((response) => {                            // 後端回傳的資訊
-        // response = {memId: 10}
-        this.member = response.data,
-        console.log(member),
-        this.memId = response.data.memId,
-        this.memAccount = response.data.memAccount,
-        this.memPassword = response.data.memPassword,
-        this.memName = response.data.memName,
-        this.memPhone = response.data.memPhone,
-        this.memNickname = response.data.memNickname,
-        this.memBirthday = response.data.memBirthday,
-        this.memGender = response.data.memGender,
-        this.memEmail= response.data.memEmail,
-        this.mailVerify = response.data.mailVerify,
-        this.memLocation = response.data.memLocation,
-        this.memPhoto = response.data.memPhoto,
-        this.interest1 = response.data.interest1,
-        this.interest2 = response.data.interest2,
-        this.interest3 = response.data.interest3,
-        this.createTime = response.data.createTime,
-        this.updateTime = response.data.updateTime,
-        this.memStatus = response.data.memStatus
-      })
-      .catch((error) => console.log(error))
-        
+      // console.log(this.memId);    
+      this.getMemberDetail();                            //
     },
     methods: {
-      edit(){
-        axios.post(this.url, {
-          memId: this.memId,
-          // memAccount: this.memAccount,
-          // memPassword: this.memPassword,
-          // memName: this.memName,
-          // memPhone: this.memPhone,
-          // memNickname: this.memNickname,
-          // memBirthday: this.memBirthday,
-          // memGender: this.memGender,
-          // memEmail: this.memEmail,
-          // mailVerify: this.mailVerify,
-          // memLocation: this.memLocation,
-          // memPhoto: this.memPhoto,
-          // interest1: this.interest1,
-          // interest2: this.interest2,
-          // interest3: this.interest3,
-          // createTime: this.createTime,
-          // updateTime: this.updateTime,
-          // memStatus: this.memStatus
+      getMemberDetail() {
+        axios.post(this.url,{                            //promise 等後端回應
+        //   headers: {
+        //     'Content-Type': 'application/x-www-form-urlencoded'
+        // },
+         
+          memAccount: this.memAccount,
+          memPassword: this.memPassword,
+          memName: this.memName,
+          memPhone: this.memPhone,
+          memNickname: this.memNickname,
+          memBirthday: this.memBirthday,
+          memGender: this.memGender,
+          memEmail: this.memEmail,
+          mailVerify: this.mailVerify,
+          memLocation: this.memLocation,
+          memPhoto: this.memPhoto,
+          interest1: this.interest1,
+          interest2: this.interest2,
+          interest3: this.interest3,
+          createTime: this.createTime,
+          updateTime: this.updateTime,
+          memStatus: this.memStatus
+      
+      })
+        .then((response) => {   
+          console.log(response.data)                         // 後端回傳的資訊
+          // response = {memId: 10}
+          this.member = response.data,
+          console.log(this.member),
+          this.memId = response.data.memId,
+          this.memAccount = response.data.memAccount,
+          this.memPassword = response.data.memPassword,
+          this.memName = response.data.memName,
+          this.memPhone = response.data.memPhone,
+          this.memNickname = response.data.memNickname,
+          this.memBirthday = response.data.memBirthday,
+          this.memGender = response.data.memGender,
+          this.memEmail= response.data.memEmail,
+          this.mailVerify = response.data.mailVerify,
+          this.memLocation = response.data.memLocation,
+          this.memPhoto = response.data.memPhoto,
+          this.interest1 = response.data.interest1,
+          this.interest2 = response.data.interest2,
+          this.interest3 = response.data.interest3,
+          this.createTime = response.data.createTime,
+          this.updateTime = response.data.updateTime,
+          this.memStatus = response.data.memStatus
         })
-      }
-        
+        .catch((error) => console.log(error))
+      },
 
+      edit(){
+        this.isDisabled = false;
+        console.log(this.memId);
+        
+        
+      },
+      //取消時
+      cancel() {
+        this.isDisabled = true;
+        this.getMemberDetail();
+      },
+
+      submit(){
+        //按送出之後要作業送出的資料
+        axios.put(this.url, {
+          //   headers: {
+          //     'Content-Type': 'application/x-www-form-urlencoded'
+          // },
+            member: this.member,
+            memId :this.memId,
+            memAccount: this.memAccount,
+            memPassword: this.memPassword,
+            memName: this.memName,
+            memPhone: this.memPhone,
+            memNickname: this.memNickname,
+            memBirthday: this.memBirthday,
+            memGender: this.memGender,
+            memEmail: this.memEmail,
+            mailVerify: this.mailVerify,
+            memLocation: this.memLocation,
+            memPhoto: this.memPhoto,
+            interest1: this.interest1,
+            interest2: this.interest2,
+            interest3: this.interest3,
+            createTime: this.createTime,
+            updateTime: this.updateTime,
+            memStatus: this.memStatus
+          
+          })
       }
+       
+
+      // }
     },
-  );
+  });
 
   window.addEventListener("load", () => {
     app.mount("#app");
