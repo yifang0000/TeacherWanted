c91f6f39b1b2e000009f72caa4b01016a50fc41a
TingLun <lisawang0219@gmail.com>
2023-06-07 22:51:01 +0800

    修改:後台訂單,行事曆

--- src/main/java/com/example/teacherwanted/member/controller/CalendarController.java
+++ src/main/java/com/example/teacherwanted/member/controller/CalendarController.java
@@ -2,6 +2,7 @@ package com.example.teacherwanted.member.controller;
 
 import com.example.teacherwanted.member.model.MemberCalendar;
 import com.example.teacherwanted.member.service.MemberCalendarService;
+import com.example.teacherwanted.register_login.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +17,9 @@ public class CalendarController {
 
     @GetMapping("/calendarGet")
     public ResponseEntity<?> calendarGet(
-            @SessionAttribute(value = "MemberId", required = false) Integer memId) {
+            @SessionAttribute(value = "userInfo", required = false) User user ) {
 
-        List<MemberCalendar> memberCalendarList = memberCalendarService.selectByMemId(memId);
+        List<MemberCalendar> memberCalendarList = memberCalendarService.selectByMemId(user.getMemId());
 
         return ResponseEntity.ok(memberCalendarList);
     }
@@ -26,9 +27,9 @@ public class CalendarController {
     @PostMapping("/calendarPost")
     public ResponseEntity<?> calendarInsert(
             @RequestBody MemberCalendar memberCalendar,
-            @SessionAttribute(value = "MemberId", required = false) Integer memId) {
+            @SessionAttribute(value = "userInfo", required = false) User user ) {
 
-        memberCalendar.setMemId(memId);
+        memberCalendar.setMemId(user.getMemId());
 
         return ResponseEntity.ok(memberCalendarService.insert(memberCalendar));
     }
@@ -36,16 +37,16 @@ public class CalendarController {
     @PutMapping ("/calendarPut")
     public ResponseEntity<?> calendarUpdate(
             @RequestBody MemberCalendar memberCalendar,
-            @SessionAttribute(value = "MemberId", required = false) Integer memId) {
-        memberCalendar.setMemId(memId);
+            @SessionAttribute(value = "userInfo", required = false) User user ) {
+        memberCalendar.setMemId(user.getMemId());
 
         return ResponseEntity.ok(memberCalendarService.update(memberCalendar));
     }
 
     @DeleteMapping ("/calendarDelete")
     public ResponseEntity<?> calendarDelete(
-            @RequestParam Integer calendarId,
-            @SessionAttribute(value = "MemberId", required = false) Integer memId) {
+            @RequestParam Integer calendarId
+           ) {
 
 
         return ResponseEntity.ok(memberCalendarService.deleteById(calendarId));
